8dab4445e20315b710433422e80175ef9e3f029f
spiltmiruku <spiltmiruku@gmail.com>
2020-02-04 00:03:50 -0700

    Added styling and content for each Component

--- portfolio-project/src/Components/Contact/Contact.js
+++ portfolio-project/src/Components/Contact/Contact.js
@@ -3,8 +3,18 @@ import './contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className='contact-box'>
+            <div className='contact-inputs'>
+            <input className='sm-input' placeholder='name' type='text'></input>
+            <input className='sm-input' placeholder='email' type='email'></input>
+            <input className='lg-input' placeholder='message' type='text'></input>
+            <div className='submit-info'>SEND</div>
+            </div>
+            <div className='contact-methods'>
+                <p>tel: 801-123-1234</p>
+                <p>email: email@email.com</p>
+                <p>Salt Lake City, UT</p>
+            </div>
         </div>
     )
 }
--- portfolio-project/src/Components/Projects/Projects.js
+++ portfolio-project/src/Components/Projects/Projects.js
@@ -1,16 +1,49 @@
 import React from "react";
 import "./projects.css";
 
+import booking from "../../resources/astraeabooking.png";
+import moon from "../../resources/astraeamoon.png";
+
+import auth from "../../resources/moneta_auth.jpg";
+import payment from "../../resources/moneta_payment.jpg";
+
+import showcaseabout from "../../resources/showcaseabout.png";
+import showcaseprojects from "../../resources/showcaseprojects.png";
+
 const Projects = () => {
   return (
-    <div className='projects-box'>
+    <div className="projects-box">
       <section className="project-section">
         <p>001</p>
         <h1>ASTRAEA</h1>
+        <a href="http://54.185.198.52/#/">
+          <img id="preview-img" src={moon} />
+        </a>
+        
+        <a href="http://54.185.198.52/#/">
+        <img id="preview-img" src={booking} />
+        </a>
+
       </section>
+
       <section className="project-section">
-        <p>001</p>
+        <p>002</p>
         <h1>MONETA</h1>
+        <img id="preview-img" src={auth} />
+        <img id="preview-img" src={payment} />
+      </section>
+
+      <section className="project-section">
+        <p>003</p>
+        <h1>SHOWCASE</h1>
+        <a href='https://spiltmilk-portfolio.firebaseapp.com/'>
+        <img id="preview-img" src={showcaseabout} />
+        </a>
+
+        <a href='https://spiltmilk-portfolio.firebaseapp.com/'>
+        <img id="preview-img" src={showcaseprojects} />
+        </a>
+
       </section>
     </div>
   );
