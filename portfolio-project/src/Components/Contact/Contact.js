3c3afcbf5b0e159700ff66ea21ab76363f73cd29
spiltmiruku <spiltmiruku@gmail.com>
2020-02-14 17:38:10 -0700

    Added links to social media with icons; Added link to moneta

--- portfolio-project/src/Components/Contact/Contact.js
+++ portfolio-project/src/Components/Contact/Contact.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react';
-import './contact.css';
+import React, { Component } from "react";
+import "./contact.css";
+const linkedin =
+  "https://astraea-project.s3-us-west-2.amazonaws.com/Contact-Icons/linkedin.png";
+const github =
+  "https://astraea-project.s3-us-west-2.amazonaws.com/Contact-Icons/github.png";
+const instagram =
+  "https://astraea-project.s3-us-west-2.amazonaws.com/Contact-Icons/instagram.svg";
 
 const Contact = () => {
-    return (
-        <div className='contact-box'>
-            <div className='contact-inputs'>
-            <input className='sm-input' placeholder='name' type='text'></input>
-            <input className='sm-input' placeholder='email' type='email'></input>
-            <input className='lg-input' placeholder='message' type='text'></input>
-            <div className='submit-info'>SEND</div>
-            </div>
-            <div className='contact-methods'>
-                <p>tel: 801-123-1234</p>
-                <p>email: email@email.com</p>
-                <p>Salt Lake City, UT</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="contact-box">
+      <div className="contact-inputs">
+        <input className="sm-input" placeholder="name" type="text"></input>
+        <input className="sm-input" placeholder="email" type="email"></input>
+        <input className="lg-input" placeholder="message" type="text"></input>
+        <div className="submit-info">SEND</div>
+      </div>
+      <div className="contact-methods">
+        {/* <p>tel: 801-123-1234</p> */}
+        {/* <p>email: email@email.com</p> */}
+        <p>Current Residence: Salt Lake City, UT</p>
+        <p>
+          Contact form is currently under construction. In the meanwhile,
+          connect with me on social media:
+        </p>
+
+        <a href="https://www.linkedin.com/in/kateqz/">
+          <img src={linkedin} alt="LinkedIn" className="contact-icon" />
+        </a>
+        <a href="https://github.com/spiltmiruku">
+          <img src={github} alt="GitHub" className="contact-icon" />
+        </a>
+        <a href="https://www.instagram.com/spiltmiruku/">
+          <img src={instagram} alt="Instagram" className="contact-icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- portfolio-project/src/Components/Projects/Projects.js
+++ portfolio-project/src/Components/Projects/Projects.js
@@ -19,31 +19,34 @@ const Projects = () => {
         <a href="http://54.185.198.52/#/">
           <img id="preview-img" src={moon} />
         </a>
-        
-        <a href="http://54.185.198.52/#/">
-        <img id="preview-img" src={booking} />
-        </a>
 
+        <a href="http://54.185.198.52/#/">
+          <img id="preview-img" src={booking} />
+        </a>
       </section>
 
       <section className="project-section">
         <p>002</p>
         <h1>MONETA</h1>
-        <img id="preview-img" src={auth} />
-        <img id="preview-img" src={payment} />
+        <a href="http://moneta.guru/#/">
+          <img id="preview-img" src={auth} />
+        </a>
+
+        <a href="http://moneta.guru/#/">
+          <img id="preview-img" src={payment} />
+        </a>
       </section>
 
       <section className="project-section">
         <p>003</p>
         <h1>SHOWCASE</h1>
-        <a href='https://spiltmilk-portfolio.firebaseapp.com/'>
-        <img id="preview-img" src={showcaseabout} />
+        <a href="https://spiltmilk-portfolio.firebaseapp.com/">
+          <img id="preview-img" src={showcaseabout} />
         </a>
 
-        <a href='https://spiltmilk-portfolio.firebaseapp.com/'>
-        <img id="preview-img" src={showcaseprojects} />
+        <a href="https://spiltmilk-portfolio.firebaseapp.com/">
+          <img id="preview-img" src={showcaseprojects} />
         </a>
-
       </section>
     </div>
   );
