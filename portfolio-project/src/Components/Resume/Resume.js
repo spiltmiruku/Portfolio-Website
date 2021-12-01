f550d67bf1bb9831e160a23ed071ce58ae841c66
spiltmiruku <spiltmiruku@gmail.com>
2021-12-01 14:26:19 -0700

    update resume

--- portfolio-project/src/Components/Resume/Resume.js
+++ portfolio-project/src/Components/Resume/Resume.js
@@ -4,9 +4,14 @@ import './resume.css';
 const Resume = () => {
 
     return (
-    <div>
-        <a href="https://astraea-project.s3-us-west-2.amazonaws.com/Kate+Zhao+Resume+Feb+19+2020.pdf">RESUME</a>
+    // <div className="resume-container">
+    //     <a href="https://astraea-project.s3-us-west-2.amazonaws.com/Kate+Zhao+Resume+Feb+19+2020.pdf">RESUME</a>
+    // </div>
+
+    <div className="resume-container">
+        <a href="https://s3.console.aws.amazon.com/s3/object/astraea-project?region=us-west-2&prefix=Kate+Zhao+Resume+Dec+2021.pdf">RESUME DEC 2021</a>
     </div>
+
     )
 }
 
