4b4ed1d7b178d11b0cfe2de4920d08027bafcd60
spiltmiruku <spiltmiruku@gmail.com>
2021-12-02 17:12:32 -0700

    update resume url

--- portfolio-project/src/Components/Resume/Resume.js
+++ portfolio-project/src/Components/Resume/Resume.js
@@ -4,16 +4,11 @@ import './resume.css';
 const Resume = () => {
 
     return (
-    // <div className="resume-container">
-    //     <a href="https://astraea-project.s3-us-west-2.amazonaws.com/Kate+Zhao+Resume+Feb+19+2020.pdf">RESUME</a>
-    // </div>
-
     <div className="resume-container">
-        <a href="https://s3.console.aws.amazon.com/s3/object/astraea-project?region=us-west-2&prefix=Kate+Zhao+Resume+Dec+2021.pdf">RESUME DEC 2021</a>
+        <a href="https://astraea-project.s3.us-west-2.amazonaws.com/Kate+Zhao+Resume+Dec+2021.pdf">RESUME DEC 2021</a>
     </div>
 
     )
 }
 
 export default Resume;
-
