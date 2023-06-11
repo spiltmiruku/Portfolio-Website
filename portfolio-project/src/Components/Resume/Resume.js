2fd5d9f9f94c57e2b18d8cecb42e1308cc4cf74c
spiltmiruku <spiltmiruku@gmail.com>
2023-06-10 21:23:56 -0600

    remove resume

--- portfolio-project/src/Components/Resume/Resume.js
+++ portfolio-project/src/Components/Resume/Resume.js
@@ -5,7 +5,6 @@ const Resume = () => {
 
     return (
     <div className="resume-container">
-        <a href="https://astraea-project.s3.us-west-2.amazonaws.com/Kate+Zhao+Resume+Dec+2021.pdf">RESUME DEC 2021</a>
     </div>
 
     )
